81c8a0cc30e84be9408b4e80b5fad0c86930294b
Steven Chang <zminsc@seas.upenn.edu>
2024-05-17 16:05:42 -0700

    Initial Commit

---
+++ Bullets/BulletsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BulletsApp.swift
+//  Bullets
+//
+//  Created by Steven Chang on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct BulletsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bullets/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bullets
+//
+//  Created by Steven Chang on 5/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
