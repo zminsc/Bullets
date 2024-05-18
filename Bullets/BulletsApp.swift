fd55b6fdc5e0b447110555e815c98124856f40a0
Steven Chang <zminsc@seas.upenn.edu>
2024-05-17 22:30:58 -0700

    Add SwiftData scaffolding to BulletApp

--- Bullets/BulletsApp.swift
+++ Bullets/BulletsApp.swift
@@ -6,13 +6,27 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct BulletsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Bullet.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .modelContainer(for: Bullet.self)
         }
+        .modelContainer(sharedModelContainer)
     }
 }
